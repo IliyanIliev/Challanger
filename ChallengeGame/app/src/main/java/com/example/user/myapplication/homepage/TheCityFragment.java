4ab3b40680370f7ad26e024705288297b3e7293c
hrisiA <hrisi_13@yahoo.com>
2015-02-05 21:49:02 +0200

    app structure

---
+++ ChallengeGame/app/src/main/java/com/example/user/myapplication/homepage/TheCityFragment.java
@@ -0,0 +1,15 @@
+package com.example.user.myapplication.homepage;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TheCityFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
---
+++ ChallengeGame/app/src/main/java/tools/DrawerItemManager.java
@@ -0,0 +1,47 @@
+package tools;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.DrawerItem;
+
+/**
+ * Created by BekiAksi1 on 2/5/2015.
+ */
+public class DrawerItemManager {
+    public static List<DrawerItem> generateDrawerItemsList(){
+        List<DrawerItem> drawerItemsList=new ArrayList<DrawerItem>();
+
+        DrawerItem drawerItem1=new DrawerItem();
+        drawerItem1.setName("Profile");
+        drawerItem1.setIcon("ic_action_group");
+        drawerItemsList.add(drawerItem1);
+
+        DrawerItem drawerItem2=new DrawerItem();
+        drawerItem2.setName("Challenges");
+        drawerItem2.setIcon("ic_action_camera");
+        drawerItemsList.add(drawerItem2);
+
+        DrawerItem drawerItem3=new DrawerItem();
+        drawerItem3.setName("Achievements");
+        drawerItem3.setIcon("ic_action_labels");
+        drawerItemsList.add(drawerItem3);
+
+        DrawerItem drawerItem4=new DrawerItem();
+        drawerItem4.setName("Ranking");
+        drawerItem4.setIcon("ic_action_good");
+        drawerItemsList.add(drawerItem4);
+
+        DrawerItem drawerItem5=new DrawerItem();
+        drawerItem5.setName("Review");
+        drawerItem5.setIcon("ic_action_search");
+        drawerItemsList.add(drawerItem5);
+
+        DrawerItem drawerItem6=new DrawerItem();
+        drawerItem6.setName("Logout");
+        drawerItem6.setIcon("ic_action_settings");
+        drawerItemsList.add(drawerItem6);
+
+        return drawerItemsList;
+    }
+}
